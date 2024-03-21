99dc47ab9aa3f478492cc69fdacc7821bc77294b
CROSO360 <cristianbonillamoreira@gmail.com>
2024-03-21 03:03:08 -0500

    asignacion de url para app movil localhost

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: import("@nestjs/common/interfaces/external/cors-options.interface").CorsOptions = {
-    origin: ['https://testing-c0624.web.app'],
+    origin: ['https://testing-c0624.web.app','http://localhost:4200'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Origin,Accept,Content-Type,Authorization',
     credentials: true,
