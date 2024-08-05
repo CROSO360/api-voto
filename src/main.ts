c659a25d667d43c7cff05f577a94f7c258c7a610
CROSO360 <cristianbonillamoreira@gmail.com>
2024-08-05 11:57:01 -0500

    lo bueno empieza

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: import("@nestjs/common/interfaces/external/cors-options.interface").CorsOptions = {
-    origin: ['https://testing-c0624.web.app','http://localhost:4200','https://test-movile.web.app'],
+    origin: ['https://testing-c0624.web.app','http://localhost:4200','https://test-movile.web.app','http://localhost:55072'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Origin,Accept,Content-Type,Authorization',
     credentials: true,
